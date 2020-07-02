70a3bf88005439f38a95f7fcce00053f1c97fe3a
Benjamin Shirley-Quirk <shirleyquirk[at]gm>
2020-07-02 21:31:56 +0100

    step0

--- impls/nim/step0_repl.nim
+++ impls/nim/step0_repl.nim
@@ -1,11 +1,20 @@
-import rdstdin
+import linenoise
+proc READ*(s:string):string = s
+proc EVAL*(s:string):string = s
+proc PRINT*(s:string):string = s
+proc rep*(s:string):string = s.READ.EVAL.PRINT
 
-proc read(str: string): string = str
+when isMainModule:
+  proc main() =
+    while true:
+      let input = readLine("user> ")
+      if input == "quit\0":
+        echo "Toodles"
+        break
+      if input==nil:
+        echo "Bye"
+        break
+      echo rep($input)
+      
+  main()
 
-proc eval(ast: string): string = ast
-
-proc print(exp: string): string = exp
-
-while true:
-  let line = readLineFromStdin("user> ")
-  echo line.read.eval.print
